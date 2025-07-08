2dfd3947a5ea75e9b91491d3f9434483663b9fa9
Khushbu Yadav <khushbuyadav1721@gmail.com>
2025-07-08 21:50:09 +0530

    Fix OAuth casing issue and Google auth visibility on Vercel

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAxBZGmqm_x3WcpPzYYhnzddyjxL2LT9os",
@@ -7,11 +7,11 @@ const firebaseConfig = {
   projectId: "blog-app-49192",
   storageBucket: "blog-app-49192.appspot.com",
   messagingSenderId: "1028560160399",
-  appId: "1:1028560160399:web:ab29323d17d8ef914ed2fa"
+  appId: "1:1028560160399:web:ab29323d17d8ef914ed2fa",
 };
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const googleProvider = new GoogleAuthProvider();
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
-export { app, auth, googleProvider };
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+export { app };
